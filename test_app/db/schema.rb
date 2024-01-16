10587d3e2a853395b7ec1c7722795f6c22405b07
DiegoFernando01 <diegofvictoria2013@gmail.com>
2024-01-16 13:03:58 -0500

    Adds firts scaffolds use

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_170526) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_175412) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "pages_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "bread"
