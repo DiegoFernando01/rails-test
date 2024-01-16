ed3da4457f250a89edd6773345e9cb822b70b87e
DiegoFernando01 <diegofvictoria2013@gmail.com>
2024-01-16 15:06:56 -0500

    Adds simple form

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_175412) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_195809) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_175412) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "bread"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.text "about_me"
+    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
